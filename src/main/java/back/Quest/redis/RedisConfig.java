291ec307c087145ceb09a13e81029161b6e0a9b9
박현우 <simuoo96@gmail.com>
2026-03-31 13:48:25 +0900

    feat : 안쓰는 import문 삭제

--- src/main/java/back/Quest/redis/RedisConfig.java
+++ src/main/java/back/Quest/redis/RedisConfig.java
@@ -1,7 +1,5 @@
 package back.Quest.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
--- src/main/java/back/Quest/security/JwtFilter.java
+++ src/main/java/back/Quest/security/JwtFilter.java
@@ -3,15 +3,12 @@ package back.Quest.security;
 import back.Quest.redis.RedisTokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
