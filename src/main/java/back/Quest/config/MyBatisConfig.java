3d6cacdb571e518f8488c5db4978fd5035e9267c
박현우 <simuoo96@gmail.com>
2026-04-01 17:29:11 +0900

    feat: mybatis 설정 및 경로 설정

--- src/main/java/back/Quest/config/MyBatisConfig.java
+++ src/main/java/back/Quest/config/MyBatisConfig.java
@@ -10,17 +10,21 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import javax.sql.DataSource;
 
 @Configuration
-@MapperScan("back.Quest.mapper") // Mapper 인터페이스가 있는 패키지 경로
+@MapperScan("back.Quest.mapper")
 public class MyBatisConfig {
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
-
-        // Mapper XML 위치 설정
+        // 2. Mapper XML 위치 설정
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/**/*.xml"));
+
+        // 이걸 설정하면 DB의 user_name이 Java의 userName으로 자동 매핑됩니다.
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sessionFactory.setConfiguration(configuration);
 
         return sessionFactory.getObject();
     }
